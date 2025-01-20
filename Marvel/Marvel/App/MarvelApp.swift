35b56515d0464cfdb7430b2740493c2aabdb11ac
jgallenrenau <jordigr.bcn@bcngmail.com>
2025-01-20 19:20:35 +0100

    Restructuring in the App folder, app icons

--- Marvel/Marvel/App/MarvelApp.swift
+++ Marvel/Marvel/App/MarvelApp.swift
@@ -1,56 +1,27 @@
 import SwiftUI
-import Core
+import HeroList
+//import HeroDetail
 
 @main
 struct MarvelApp: App {
+    @State private var selectedHero: Hero?
+
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .onAppear {
-                    AppInitializer.initializeKeys()
+            NavigationView {
+                HeroListCoordinator(
+                    onHeroSelected: { hero in
+                        self.selectedHero = hero
+                    }
+                )
+                .navigationTitle("Heroes")
+                .sheet(item: $selectedHero) { hero in
+                    // TODO: HeroDetailCoordinator(hero: hero)
                 }
-        }
-    }
-}
-
-extension MarvelApp {
-    
-    struct AppInitializer {
-        static func initializeKeys() {
-            do {
-                let keys = try fetchKeysFromPlist()
-                
-                let predefinedKeys = ["MARVEL_PUBLIC_KEY", "MARVEL_PRIVATE_KEY"]
-
-                for (index, value) in keys.enumerated() {
-                    try saveKeyIfNeeded(key: predefinedKeys[index], value: value)
-                }
-            } catch {
-                print("Failed to initialize keys in Keychain: \(error) 😫")
+            }
+            .onAppear {
+                AppInitializer.initializeKeys()
             }
         }
-
-
-        private static func fetchKeysFromPlist() throws -> [String] {
-            guard let infoDictionary = Bundle.main.infoDictionary,
-                  let publicKey = infoDictionary["MARVEL_PUBLIC_KEY"] as? String,
-                  let privateKey = infoDictionary["MARVEL_PRIVATE_KEY"] as? String else {
-                throw AppInitializerError.missingPlistEntry
-            }
-            return [publicKey, privateKey]
-        }
-
-        private static func saveKeyIfNeeded(key: String, value: String) throws {
-            if (try? KeychainHelper.retrieve(key: key)) == nil {
-                try KeychainHelper.save(key: key, value: value)
-                print("\(key) saved to Keychain 🔐 !")
-            } else {
-                print("We have keys in Keychain 🔑 !")
-            }
-        }
-    }
-
-    enum AppInitializerError: Error {
-        case missingPlistEntry
     }
 }
---
+++ Marvel/Marvel/App/Utils/AppInitializer.swift
@@ -0,0 +1,42 @@
+import SwiftUI
+import Core
+
+struct AppInitializer {
+    
+    static func initializeKeys() {
+        do {
+            let keys = try fetchKeysFromPlist()
+            
+            let predefinedKeys = ["MARVEL_PUBLIC_KEY", "MARVEL_PRIVATE_KEY"]
+
+            for (index, value) in keys.enumerated() {
+                try saveKeyIfNeeded(key: predefinedKeys[index], value: value)
+            }
+        } catch {
+            print("Failed to initialize keys in Keychain: \(error) 😫")
+        }
+    }
+
+
+    private static func fetchKeysFromPlist() throws -> [String] {
+        guard let infoDictionary = Bundle.main.infoDictionary,
+              let publicKey = infoDictionary["MARVEL_PUBLIC_KEY"] as? String,
+              let privateKey = infoDictionary["MARVEL_PRIVATE_KEY"] as? String else {
+            throw AppInitializerError.missingPlistEntry
+        }
+        return [publicKey, privateKey]
+    }
+
+    private static func saveKeyIfNeeded(key: String, value: String) throws {
+        if (try? KeychainHelper.retrieve(key: key)) == nil {
+            try KeychainHelper.save(key: key, value: value)
+            print("\(key) saved to Keychain 🔐 !")
+        } else {
+            print("We have keys in Keychain 🔑 !")
+        }
+    }
+}
+
+enum AppInitializerError: Error {
+    case missingPlistEntry
+}
