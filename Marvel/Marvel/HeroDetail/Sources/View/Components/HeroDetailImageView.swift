0bc34e777c432dc8b1765ee69d34dba34bae620c
jgallenrenau <jordigr.bcn@bcngmail.com>
2025-01-22 15:33:41 +0100

    Comment test ( at the moment)

--- Marvel/Marvel/HeroDetail/Sources/View/Components/HeroDetailImageView.swift
+++ Marvel/Marvel/HeroDetail/Sources/View/Components/HeroDetailImageView.swift
@@ -8,21 +8,21 @@ struct HeroDetailImageView: View {
             switch phase {
             case .empty:
                 ProgressView()
-                    .frame(width: 200, height: 200)
+                    .frame(width: 400, height: 400)
                     .background(Color.gray.opacity(0.3))
                     .cornerRadius(10)
             case .success(let image):
                 image
                     .resizable()
                     .scaledToFill()
-                    .frame(width: 200, height: 200)
+                    .frame(width: 400, height: 400)
                     .clipShape(RoundedRectangle(cornerRadius: 10))
                     .shadow(radius: 5)
             case .failure:
                 Image(systemName: "photo")
                     .resizable()
                     .scaledToFit()
-                    .frame(width: 200, height: 200)
+                    .frame(width: 400, height: 400)
                     .foregroundColor(.gray)
                     .background(Color.gray.opacity(0.3))
                     .cornerRadius(10)
