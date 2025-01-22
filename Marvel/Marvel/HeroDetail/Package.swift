42c22e77685db7e7d85b0a9acaff3ce607adf066
jgallenrenau <jordigr.bcn@bcngmail.com>
2025-01-22 09:54:25 +0100

    SPM Hero Detail - Creating the Hero Detail package

---
+++ Marvel/Marvel/HeroDetail/Package.swift
@@ -0,0 +1,36 @@
+// swift-tools-version: 5.9
+
+import PackageDescription
+
+let package = Package(
+    name: "HeroDetail",
+    platforms: [.iOS(.v17)],
+    products: [
+        .library(
+            name: "HeroDetail",
+            targets: ["HeroDetail"]
+        )
+    ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", from: "1.17.1"),
+        .package(name: "Core", path: "../Core"),
+        .package(url: "https://github.com/pointfreeco/swift-snapshot-testing", from: "1.11.0")
+    ],
+    targets: [
+        .target(
+            name: "HeroDetail",
+            dependencies: [
+                "Core",
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            ]
+        ),
+        .testTarget(
+            name: "HeroDetailTests",
+            dependencies: [
+                "HeroDetail",
+                "Core",
+                .product(name: "SnapshotTesting", package: "swift-snapshot-testing")
+            ]
+        )
+    ]
+)
