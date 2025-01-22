f603bf97c1f8c22fb2c5a231fab9381d7107bb45
jgallenrenau <jordigr.bcn@bcngmail.com>
2025-01-22 22:53:32 +0100

    Core: Refactoring KeyChain

---
+++ Marvel/Marvel/Core/Sources/Security/KeychainHelper.swift
@@ -0,0 +1,50 @@
+import Foundation
+import Security
+
+public class KeychainHelper: KeychainHelperProtocol {
+    
+    public init() {}
+
+    public func save(key: String, value: String) throws {
+        let data = value.data(using: .utf8)!
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecValueData as String: data
+        ]
+
+        SecItemDelete(query as CFDictionary)
+        let status = SecItemAdd(query as CFDictionary, nil)
+        guard status == errSecSuccess else {
+            throw KeychainError.unableToSave
+        }
+    }
+
+    public func retrieve(key: String) throws -> String {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key,
+            kSecReturnData as String: kCFBooleanTrue!,
+            kSecMatchLimit as String: kSecMatchLimitOne
+        ]
+
+        var result: AnyObject?
+        let status = SecItemCopyMatching(query as CFDictionary, &result)
+
+        guard status == errSecSuccess, let data = result as? Data, let value = String(data: data, encoding: .utf8) else {
+            throw KeychainError.unableToRetrieve
+        }
+        return value
+    }
+
+    public func delete(key: String) throws {
+        let query: [String: Any] = [
+            kSecClass as String: kSecClassGenericPassword,
+            kSecAttrAccount as String: key
+        ]
+        let status = SecItemDelete(query as CFDictionary)
+        guard status == errSecSuccess || status == errSecItemNotFound else {
+            throw KeychainError.unableToDelete
+        }
+    }
+}
