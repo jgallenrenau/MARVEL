fbeb0c7d2ae3f4286b54ddf617ef3ca70c58dd93
jgallenrenau <jordigr.bcn@bcngmail.com>
2025-01-22 22:56:33 +0100

    Core: New way to initialize the app

--- Marvel/Marvel/App/Utils/AppInitializer.swift
+++ Marvel/Marvel/App/Utils/AppInitializer.swift
@@ -2,41 +2,47 @@ import SwiftUI
 import Core
 
 struct AppInitializer {
-    
+
+    private static let keychainHelper: KeychainHelperProtocol = KeychainHelper()
+
     static func initializeKeys() {
         do {
             let keys = try fetchKeysFromPlist()
-            
-            let predefinedKeys = ["MARVEL_PUBLIC_KEY", "MARVEL_PRIVATE_KEY"]
+            let predefinedKeys = PredefinedKeys.allCases
 
             for (index, value) in keys.enumerated() {
-                try saveKeyIfNeeded(key: predefinedKeys[index], value: value)
+                try saveKeyIfNeeded(key: predefinedKeys[index].rawValue, value: value)
             }
         } catch {
-            print("Failed to initialize keys in Keychain: \(error) 😫")
+            print("❌ Failed to initialize keys in Keychain: \(error)")
         }
     }
 
-
     private static func fetchKeysFromPlist() throws -> [String] {
         guard let infoDictionary = Bundle.main.infoDictionary,
-              let publicKey = infoDictionary["MARVEL_PUBLIC_KEY"] as? String,
-              let privateKey = infoDictionary["MARVEL_PRIVATE_KEY"] as? String else {
+              let publicKey = infoDictionary[PredefinedKeys.publicKey.rawValue] as? String,
+              let privateKey = infoDictionary[PredefinedKeys.privateKey.rawValue] as? String else {
             throw AppInitializerError.missingPlistEntry
         }
         return [publicKey, privateKey]
     }
 
     private static func saveKeyIfNeeded(key: String, value: String) throws {
-        if (try? KeychainHelper.retrieve(key: key)) == nil {
-            try KeychainHelper.save(key: key, value: value)
-            print("\(key) saved to Keychain 🔐 !")
+        if (try? keychainHelper.retrieve(key: key)) == nil {
+            try keychainHelper.save(key: key, value: value)
+            print("✅ \(key) saved to Keychain 🔐")
         } else {
-            print("We have keys in Keychain 🔑 !")
+            print("ℹ️ \(key) already exists in Keychain 🔑")
         }
     }
+}
+
+private enum PredefinedKeys: String, CaseIterable {
+    case publicKey = "MARVEL_PUBLIC_KEY"
+    case privateKey = "MARVEL_PRIVATE_KEY"
 }
 
 enum AppInitializerError: Error {
     case missingPlistEntry
 }
+
