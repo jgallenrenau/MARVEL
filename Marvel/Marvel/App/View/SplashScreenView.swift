55942600eaf0292ca82aa4b85bac6d6113bd5779
jgallenrenau <jordigr.bcn@bcngmail.com>
2025-01-28 19:10:39 +0100

    Animated-Splash-Screen: Make the image fit the width of the screen

--- Marvel/Marvel/App/View/SplashScreenView.swift
+++ Marvel/Marvel/App/View/SplashScreenView.swift
@@ -5,8 +5,8 @@ struct SplashScreenView: View {
     
     var onAnimationEnd: () -> Void
     
-    @State private var scaleEffect = 0.8
-    @State private var opacity = 0.0
+    @State private var scaleEffect = 0.0
+    @State private var opacity = DSOpacity.zero
 
     var body: some View {
         ZStack {
@@ -29,35 +29,37 @@ struct SplashScreenView: View {
     // MARK: - Content View
     @ViewBuilder
     private func contentView() -> some View {
-        VStack {
-            Spacer()
-            animatedLogo()
-            Spacer()
+        GeometryReader { geometry in
+            VStack {
+                Spacer()
+                animatedLogo(maxWidth: geometry.size.width)
+                Spacer()
+            }
         }
     }
     
     // MARK: - Animated Logo
     @ViewBuilder
-    private func animatedLogo() -> some View {
+    private func animatedLogo(maxWidth: CGFloat) -> some View {
         Image("marvel_logo")
             .resizable()
             .scaledToFit()
-            .frame(width: 100, height: 100)
+            .frame(width: maxWidth)
             .scaleEffect(scaleEffect)
             .opacity(opacity)
     }
     
     // MARK: - Animations
     private func startAnimations() {
-        withAnimation(.easeInOut(duration: 3.5)) {
-            self.scaleEffect = 5
-            self.opacity = 1.0
+        withAnimation(.easeInOut(duration: DSTimeAnimation.slowXXL)) {
+            self.scaleEffect = 1.2
+            self.opacity = DSOpacity.one
         }
     }
     
     // MARK: - Navigation
     private func navigateAfterDelay() {
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
+        DispatchQueue.main.asyncAfter(deadline: .now() + DSTimeAnimation.slowXXL) {
             self.onAnimationEnd()
         }
     }
